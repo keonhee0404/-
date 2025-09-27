d66076e4d5439ca44e87cec2c47007955940a46c
gunhee0404 <gunhee041108@gmail.com>
2025-09-28 00:25:38 +0900

    'finally commit!'

---
+++ thisisjava/src/test/test.java
@@ -0,0 +1,26 @@
+package test;
+
+public class test {
+
+	public static void main(String[] args) {
+		
+		
+		int x = 1000000;
+		int y = 1000000;
+		
+		int z = x*y;
+		
+		System.out.println(z);
+		System.out.println("-----------------------");
+		
+		
+		long x1= 100000;
+		long y1= 100000;
+		long z1= (long)x*(long)y;
+		
+		System.out.println(z1);
+		
+		
+	}
+
+}
