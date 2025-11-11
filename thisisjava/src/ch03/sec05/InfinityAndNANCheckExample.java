630f443a12d15a7ca67fc5f4d7d558bf5aa46a23
gunhee0404 <gunhee041108@gmail.com>
2025-11-11 15:33:23 +0900

    'week9commit

--- thisisjava/src/ch03/sec05/InfinityAndNANCheckExample.java
+++ thisisjava/src/ch03/sec05/InfinityAndNANCheckExample.java
@@ -5,7 +5,8 @@ public class InfinityAndNANCheckExample {
 	public static void main(String[] args) {
 		int x = 5;
 		double y = 0.0;
-		double z = x/y;
+		double z = x%y;
+		
 		
 		System.out.println(z+2);
 		
---
+++ thisisjava/src/ch06/sec03/SportsCar.java
@@ -0,0 +1,14 @@
+package ch06.sec03;
+
+public class SportsCar {
+
+	public static void main(String[] args) {
+	
+	}
+	
+	class Tire {
+		
+		
+	}
+
+}
